f71e56818f4066eb19b2b122993eef818bf2311d
Fyustorm <fyustorm@gmail.com>
2023-06-16 23:27:09 +0200

    Update to minecraft 1.20.1

--- src/main/java/fr/fyustorm/minetiface/fabric/mixins/MinetifaceGuiMixin.java
+++ src/main/java/fr/fyustorm/minetiface/fabric/mixins/MinetifaceGuiMixin.java
@@ -1,5 +1,6 @@
 package fr.fyustorm.minetiface.fabric.mixins;
 
+import net.minecraft.client.gui.DrawContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -7,17 +8,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import fr.fyustorm.minetiface.gui.GuiMinetiface;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
 
 @Mixin(InGameHud.class)
 public abstract class MinetifaceGuiMixin {
 
     private static GuiMinetiface gui;
 
-    @Inject(method = "render", at = @At("RETURN"), cancellable = true) 
-    private void onRender(MatrixStack stack, float tickDelta, CallbackInfo ci) {
+    @Inject(method = "render", at = @At("RETURN"), cancellable = true)
+    public void render(DrawContext context, float tickDelta, CallbackInfo ci) {
         if (gui == null)
             gui = new GuiMinetiface();
-        gui.onRender(stack, tickDelta);
+        gui.onRender(context);
     }
 }
--- src/main/java/fr/fyustorm/minetiface/gui/GuiMinetiface.java
+++ src/main/java/fr/fyustorm/minetiface/gui/GuiMinetiface.java
@@ -5,33 +5,32 @@ import fr.fyustorm.minetiface.intiface.MinetifaceController;
 import fr.fyustorm.minetiface.intiface.ToyController;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.gui.DrawContext;
+
+import java.text.DecimalFormat;
 
 public class GuiMinetiface {
-    private final MinecraftClient minecraft;
+
     private final TextRenderer fontRenderer;
 
     public GuiMinetiface() {
-        minecraft = MinecraftClient.getInstance();
-        fontRenderer = minecraft.textRenderer;
+        fontRenderer = MinecraftClient.getInstance().textRenderer;
     }
 
-    public void onRender(MatrixStack stack, float partialTicks) {
+    public void onRender(DrawContext context) {
         if (!MinetifaceConfig.INSTANCE.showIntensity) {
             return;
         }
 
         MinetifaceController client = MinetifaceController.getInstance();
-        
 
+
+        DecimalFormat df = new DecimalFormat("0.00");
         int intensity = Math.round(client.getIntensity() * 100);
         String strIntensity = "Intensity " + intensity + "%";
-        String strPosition = "Position " + ToyController.instance().getLinearPosition();
+        String strPosition = "Position " + df.format(ToyController.instance().getLinearPosition());
 
-        stack.scale(0.8F, 0.8F, 0.8F);
-        fontRenderer.draw(stack, strIntensity, 2, 2, MinetifaceConfig.INSTANCE.intensityColor);
-        fontRenderer.draw(stack, strPosition, 2, 12, MinetifaceConfig.INSTANCE.intensityColor);        
-        //fontRenderer.draw(stack, "Duration " + client.getSkipDownTicks() / 20 + "s", 2, 22, 0xFF00FF);
-        stack.scale(1.25F, 1.25F, 1.25F);
+        context.drawTextWithShadow(fontRenderer, strIntensity, 2, 2, MinetifaceConfig.INSTANCE.intensityColor);
+        context.drawTextWithShadow(fontRenderer, strPosition, 2, 12, MinetifaceConfig.INSTANCE.intensityColor);
     }
 }
