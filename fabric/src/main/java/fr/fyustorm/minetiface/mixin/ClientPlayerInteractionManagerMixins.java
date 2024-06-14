10400e5e3450d5401656ad350eed04cb0bdaed0b
Fyustorm <fyustorm@gmail.com>
2024-06-14 20:32:37 +0200

    Fix fabric compatibility with Minecraft 1.20.5+

--- fabric/src/main/java/fr/fyustorm/minetiface/mixin/ClientPlayerInteractionManagerMixins.java
+++ fabric/src/main/java/fr/fyustorm/minetiface/mixin/ClientPlayerInteractionManagerMixins.java
@@ -18,6 +18,8 @@ import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.AttributeModifiersComponent;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
@@ -25,6 +27,9 @@ import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -59,28 +64,26 @@ public class ClientPlayerInteractionManagerMixins {
 
     @Inject(method = "attackEntity(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/entity/Entity;)V", at = @At("TAIL"))
     public void afterAttackEntity(PlayerEntity player, Entity target, CallbackInfo ci) {
-        LOGGER.info("Attack after");
         double value = 1;
         if (target instanceof LivingEntity livingEntity) {
             ItemStack item = player.getMainHandStack();
 
+            AttributeModifiersComponent attributes = item.get(DataComponentTypes.ATTRIBUTE_MODIFIERS);
+            if (attributes != null) {
+                // We are searching the item damage attribute
+                Identifier damagIdentifier = new Identifier("minecraft:generic.attack_damage");
+                for (AttributeModifiersComponent.Entry entry : attributes.modifiers()) {
+                    RegistryKey<EntityAttribute> registryKey = entry.attribute().getKey().orElse(null);
+                    Identifier identifier = registryKey.getValue();
 
-            for (Map.Entry<EntityAttribute, Collection<EntityAttributeModifier>> keyPair : item.getAttributeModifiers(EquipmentSlot.MAINHAND).asMap().entrySet()) {
-                EntityAttribute attribute = keyPair.getKey();
-                Collection<EntityAttributeModifier> modifiers = keyPair.getValue();
-
-                if (!"attribute.name.generic.attack_damage".equals(attribute.getTranslationKey())) {
-                    continue;
-                }
-
-                LOGGER.debug("Attribute = {}", attribute.getTranslationKey());
-                for (EntityAttributeModifier modifier : modifiers) {
-                    value = modifier.getValue();
-//                    LOGGER.debug("-- Modifier {} Value {}", modifier.getName(), modifier.getValue());
-                    break;
-
+                    if (damagIdentifier.equals(identifier)) {
+                        value = entry.modifier().value();
+                        break;
+                    }
                 }
             }
+
+            // If we killed the entity, increase the value
             if (livingEntity.getHealth() < value) {
                 value *= 1.5;
             }
